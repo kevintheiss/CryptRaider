94dfd468fb369f076264ad1b4c4a99a2f265afd7
kevintheiss <ktheiss1990@gmail.com>
2023-10-24 10:20:17 -0700

    Added a constructor and TickComponent function to the TriggerComponent.

--- CryptRaider/Source/CryptRaider/TriggerComponent.cpp
+++ CryptRaider/Source/CryptRaider/TriggerComponent.cpp
@@ -3,6 +3,17 @@
 
 #include "TriggerComponent.h"
 
+// Sets default values for this component's properties
+UTriggerComponent::UTriggerComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+	UE_LOG(LogTemp, Display, TEXT("UTriggerComponent is constructed"));
+}
+
 // Called when the game starts
 void UTriggerComponent::BeginPlay()
 {
@@ -11,4 +22,9 @@ void UTriggerComponent::BeginPlay()
 	UE_LOG(LogTemp, Display, TEXT("Trigger Component Alive"));
 }
 
+// Called every frame
+void UTriggerComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+}
 
